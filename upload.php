a8f17577b4a283228b1e411e94ab0d0f11313ad6
rivaldo <rivaldojeffmarvin0@gmail.com>
2024-08-07 17:11:38 +0700

    first commit

---
+++ upload.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>percobaan git</title>
+</head>
+<body>
+    <h2>Hallo ini percobaan github</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio cum voluptates numquam aliquid similique possimus beatae, omnis dolorem? Aliquid eum aperiam id corporis possimus! Nam aut dolores a maxime sequi.</p>
+</body>
+</html>
